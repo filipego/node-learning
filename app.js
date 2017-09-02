c4016101edc5fcc8bcbff4652afa510f005d2995
Filipe Valente Gomes <Filipe@Filipes-iMac.fios-router.home>
2017-09-02 01:18:19 -0400

    added list command

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
@@ -8,8 +6,6 @@ const notes = require('./notes.js')
 
 const argv = yargs.argv;
 let command = argv._[0]
-console.log('Command', command);
-console.log('Yargs', argv);
 
 if (command === 'add') {
   let note = notes.addNote(argv.title, argv.body);
@@ -20,7 +16,9 @@ if (command === 'add') {
     console.log('Note title taken')
   }
 } else if (command === 'list') {
-  notes.getAll();
+  let allNotes = notes.getAll();
+  console.log(`Printing ${allNotes.length} note(s).`);
+  allNotes.forEach((note) => notes.logNote(note));
 } else if (command === 'read') {
   let note = notes.getNote(argv.title);
   if (note) {
